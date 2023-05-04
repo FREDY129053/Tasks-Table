8e1eab9bc7527d05c24cd237ab0e113697a216b2
FREDY129053 <83633741+FREDY129053@users.noreply.github.com>
2023-05-04 11:12:22 +1000

    Окна для реги/входа(без доп проверки сложности)

--- project.py
+++ project.py
@@ -4,12 +4,13 @@ import mysql
 from secrets import compare_digest
 
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import (
-    QApplication, QWidget, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QSizePolicy, QMainWindow,
-    QGridLayout, QLabel, QStatusBar, QCheckBox
-)
+from PyQt6.QtWidgets import (QApplication, QWidget, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QSizePolicy,
+                             QMainWindow, QGridLayout, QLabel, QStatusBar, QCheckBox, QDialog, QStackedWidget
+                             )
 from PyQt6.QtCore import Qt
 from PyQt6 import QtWidgets
+from PyQt6.uic import loadUi
+from PyQt6.QtCore import QCoreApplication
 from mysql.connector import connect
 
 
@@ -28,9 +29,11 @@ class Main(QMainWindow):
         menubar = self.menuBar()
         file_menu = menubar.addMenu("File")
         file_menu.addAction(add_table_action)
+
     def add_table(self):
         self.main = AddTable()
         self.main.show()
+
 
 # Создание новой доски
 class AddTable(QWidget):
@@ -102,92 +105,129 @@ class AddTable(QWidget):
             print(public_private)
             self.close()
 
-
-# Окно регистрации/входа(отдельное окно)
-class Login(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Вход/Регистрация")
-        self.setFixedSize(450, 150)
-
         layout = QGridLayout()
         self.setLayout(layout)
 
-        # все виджеты окна
-        labels = {}
-        self.lineEdits = {}
+        button_ = QPushButton("Зарегестрироваться")
+        layout.addWidget(button_, 2, 2, 1, 1)
 
-        labels['Username'] = QLabel("Имя")
-        labels['Password'] = QLabel("Пароль")
-        labels['Username'].setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
-        labels['Password'].setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        cont = QWidget()
+        cont.setLayout(layout)
 
-        self.lineEdits['Username'] = QLineEdit()
-        self.lineEdits['Password'] = QLineEdit()
-        self.lineEdits['Password'].setEchoMode(QLineEdit.EchoMode.Password)
+        self.setCentralWidget(cont)
 
-        layout.addWidget(labels['Username'], 0, 0, 1, 1)
-        layout.addWidget(self.lineEdits['Username'], 0, 1, 1, 3)
-        layout.addWidget(labels['Password'], 1, 0, 1, 1)
-        layout.addWidget(self.lineEdits['Password'], 1, 1, 1, 3)
 
-        # кнопки
-        button_sign_up = QPushButton("Зарегестрироваться")
-        layout.addWidget(button_sign_up, 2, 2, 1, 1)
-        button_sign_up.clicked.connect(self.sign_up)
+# Окно регистрации/входа(отдельное окно)
+class signUp(QDialog):
+    def __init__(self):
+        super(signUp, self).__init__()
+        loadUi("signup.ui", self)
+        self.setWindowTitle("Вход/Регистрация")
 
-        button_login = QPushButton("Войти")
-        button_login.clicked.connect(self.login)
-        layout.addWidget(button_login, 2, 3, 1, 1)
+        # кнопка перехода в окно "ВХОД"
+        self.login.clicked.connect(self.log_in)
 
-        self.button_see_password = QPushButton("🙈")
-        self.button_see_password.clicked.connect(self.show_pass)
-        layout.addWidget(self.button_see_password, 1, 4, 1, 1)
+        # кнопка и поле для пароля
+        self.show_hide_pass_btn.setText("🙈")
+        self.show_hide_pass_btn.clicked.connect(self.show_pass)
+        self.password.setEchoMode(QLineEdit.EchoMode.Password)
 
-        self.status = QLabel('')
-        self.status.setStyleSheet("font-size: 20px; color: red;")
-        layout.addWidget(self.status, 3, 2, 1, 1)
+        # кнопка и поле для подтверждения пароля
+        self.show_hide_pass_btn_2.setText("🙈")
+        self.show_hide_pass_btn_2.clicked.connect(self.show_pass_2)
+        self.confirm_password.setEchoMode(QLineEdit.EchoMode.Password)
+
+        # кнопка регистрации
+        self.registration_btn.clicked.connect(self.sign_up)
 
     def sign_up(self):
         # добавить проверки на пустоту и на пароль!
 
-        username = self.lineEdits['Username'].text()
-        password = self.lineEdits['Password'].text()
+        username = self.username.text()
+        password = self.password.text()
+        confirm_password = self.confirm_password.text()
 
-        db_config = {
-            "user": "me",
-            "password": "password",
-            "host": "193.124.118.138",
-            "database": "task_table",
-        }
-        database = mysql.connector.connect(**db_config)
-        cursor = database.cursor(buffered=True)
+        # count_of_spec_symb = password.count('~', '!','@','#','$','%','^','&','*','(',')','+','`', ';',':','<','>','/','\\',''|',')
+        # print(count_of_spec_symb)
 
-        cursor.execute(f"SELECT * FROM users WHERE login='{username}'")
-        info = cursor.fetchall()
+        # роверка на пустоту
+        # if username == "" or password == "" or confirm_password == "":
+        #     self.status.setText("Заполните все поля!")
+        if compare_digest(password, confirm_password):
+            db_config = {
+                "user": "me",
+                "password": "password",
+                "host": "193.124.118.138",
+                "database": "task_table",
+            }
+            database = mysql.connector.connect(**db_config)
+            cursor = database.cursor(buffered=True)
 
-        if len(info) == 0:
-            cursor.execute(f"INSERT INTO users(login, password) VALUES('{username}', '{password}')")
-            database.commit()
-            self.main = Main()
-            self.main.show()
-            self.close()
+            cursor.execute(f"SELECT * FROM users WHERE login='{username}'")
+            info = cursor.fetchall()
+
+            if len(info) == 0:
+                cursor.execute(f"INSERT INTO users(login, password) VALUES('{username}', '{password}')")
+                database.commit()
+
+                self.main = Main()
+                self.main.show()
+            else:
+                self.status.setText("Данное имя занято!")
         else:
-            self.status.setText("Choose another name!")
+            self.status.setText("Пароли не совпадают!")
+
+    def log_in(self):
+        self.close()
+        self.logIn = logIn()
+        self.logIn.show()
 
     def show_pass(self):
-        echo_mode = self.lineEdits['Password'].echoMode()
+        echo_mode = self.password.echoMode()
 
         if echo_mode == QLineEdit.EchoMode.Normal:
-            self.button_see_password.setText("🙈")
-            self.lineEdits['Password'].setEchoMode(QLineEdit.EchoMode.Password)
+            self.show_hide_pass_btn.setText("🙈")
+            self.password.setEchoMode(QLineEdit.EchoMode.Password)
         else:
-            self.button_see_password.setText("🐵")
-            self.lineEdits['Password'].setEchoMode(QLineEdit.EchoMode.Normal)
+            self.show_hide_pass_btn.setText("🐵")
+            self.password.setEchoMode(QLineEdit.EchoMode.Normal)
 
-    def login(self):
-        username = self.lineEdits['Username'].text()
-        password = self.lineEdits['Password'].text()
+    def show_pass_2(self):
+        echo_mode = self.confirm_password.echoMode()
+
+        if echo_mode == QLineEdit.EchoMode.Normal:
+            self.show_hide_pass_btn_2.setText("🙈")
+            self.confirm_password.setEchoMode(QLineEdit.EchoMode.Password)
+        else:
+            self.show_hide_pass_btn_2.setText("🐵")
+            self.confirm_password.setEchoMode(QLineEdit.EchoMode.Normal)
+
+
+class logIn(QDialog):
+    def __init__(self):
+        super(logIn, self).__init__()
+        loadUi("login.ui", self)
+        self.setWindowTitle("Вход/Регистрация")
+
+        # кнопка перехода в окно "РЕГИСТРАЦИЯ"
+        self.signup.clicked.connect(self.sign_up)
+
+        # кнопка и поле для пароля
+        self.show_hide_pass_btn.setText("🙈")
+        self.show_hide_pass_btn.clicked.connect(self.show_pass)
+        self.password.setEchoMode(QLineEdit.EchoMode.Password)
+
+        # кнопка входа
+        self.login_btn.clicked.connect(self.log_in)
+
+    def sign_up(self):
+        self.close()
+        self.signUp = signUp()
+        self.signUp.show()
+
+    def log_in(self):
+        username = self.username.text()
+        password = self.password.text()
 
         db_config = {
             "user": "me",
@@ -207,13 +247,28 @@ class Login(QWidget):
             if password != user_info[0][1]:
                 self.status.setText("Wrong password!")
             else:
+                self.close()
                 self.main = Main()
                 self.main.show()
-                self.close()
+
+
+    def show_pass(self):
+        echo_mode = self.password.echoMode()
+
+        if echo_mode == QLineEdit.EchoMode.Normal:
+            self.show_hide_pass_btn.setText("🙈")
+            self.password.setEchoMode(QLineEdit.EchoMode.Password)
+        else:
+            self.show_hide_pass_btn.setText("🐵")
+            self.password.setEchoMode(QLineEdit.EchoMode.Normal)
 
 
 app = QApplication(sys.argv)
-window = Login()
+window = logIn()
+# widget = QStackedWidget()
+# widget.addWidget(window)
+# widget.setFixedSize(500, 500)
+# widget.show()
 window.show()
 
 app.exec()
