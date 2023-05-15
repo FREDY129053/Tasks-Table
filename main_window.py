3443cabc9fd2a5d702cf3b7f09fd2abd1a96b6df
FREDY129053 <83633741+FREDY129053@users.noreply.github.com>
2023-05-15 15:30:19 +1000

    Вывод таблиц + меню примерных функций

--- main_window.py
+++ main_window.py
@@ -23,15 +23,26 @@ class Main(QMainWindow):
 
         self.setWindowTitle("Main")
 
+        btn = self.findChild(QPushButton, 'add_table_btn')
         self.add_table_btn.clicked.connect(self.add_table)
 
         table = self.findChild(QTableWidget, 'tableWidget')
         scroll_area = self.findChild(QScrollArea, 'scrollArea')
+
+        parent = scroll_area.parentWidget()
+        btn.move(650, 513)
+        btn.setParent(parent)
+
         scroll_layout = QVBoxLayout()
         scroll_widget = QWidget()
 
-        for i in range(100):
+        for _ in range(100):
             new = QTableWidget()
+            name = QLabel()
+            name.setText("TABLES")
+            scroll_layout.addWidget(name)
+            new.setFixedWidth(740)
+            new.setFixedHeight(300)
             new.setColumnCount(3)
             new.setHorizontalHeaderLabels(['Надо сделать', 'В процессе', 'Готово!'])
             self.load_data(new)
@@ -41,10 +52,10 @@ class Main(QMainWindow):
             scroll_widget.setLayout(scroll_layout)
             scroll_area.setWidget(scroll_widget)
 
-
             # new.setGeometry(table.geometry().x(), table.geometry().y() + (50 + table.height()) * i, table.width(), table.height())
             # new.setParent(self)
 
+    # заполнение таблицы данными
     def load_data(self, table):
         tasks = [{"name": "Посрать", "age": "Покупка туалетки", "address": "Срать"},
                  {"name": "Mark", "age": "18", "address": "Alabama"},
