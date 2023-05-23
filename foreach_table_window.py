f651cc5d0d4522c66a9f9a25a346fb123d6b3283
FREDY129053 <83633741+FREDY129053@users.noreply.github.com>
2023-05-23 23:34:17 +1000

    Кривой дизайн вам

--- foreach_table_window.py
+++ foreach_table_window.py
@@ -32,17 +32,32 @@ cursor = database.cursor(buffered=True)
 
 
 class EachTable(QWidget):
-    def __init__(self):
+    def __init__(self, window_title):
         super(EachTable, self).__init__()
         loadUi("Users_Interfaces/tables_widget.ui", self)
+        self.setWindowTitle(window_title)
 
-        self.add_column_btn.clicked.connect(self.add_column)
+        name = self.findChild(QLabel, 'label_2')
+        author = self.findChild(QLabel, 'label')
+        coauthor = self.findChild(QLabel, 'label_3')
 
-        table = self.findChild(QTableWidget)
-        print(self.windowTitle())
-        cursor.execute(f"SELECT size_of_table FROM tables WHERE table_name = '{self.windowTitle()}'")
-        size_of_current_table = cursor.fetchall()
-        print(size_of_current_table)
+        name.setText(self.windowTitle())
+
+        cursor.execute(f"SELECT author_login FROM tables WHERE table_name = '{self.windowTitle()}'")
+        author_from_db = cursor.fetchone()
+        author.setText(author_from_db[0])
+
+        cursor.execute(f"SELECT coauthors_login FROM tables WHERE table_name = '{self.windowTitle()}'")
+        coauthors_from_db = cursor.fetchone()
+        coauthor.setText(coauthors_from_db[0])
+
+        # self.add_column_btn.clicked.connect(self.add_column)
+
+        # table = self.findChild(QTableWidget)
+        # print(self.windowTitle())
+        # cursor.execute(f"SELECT size_of_table FROM tables WHERE table_name = '{self.windowTitle()}'")
+        # size_of_current_table = cursor.fetchall()
+        # print(size_of_current_table)
 
     def add_column(self):
         cursor.execute(f"UPDATE tables SET size_of_table = size_of_table + 1 WHERE table_name = '{self.windowTitle()}'")
