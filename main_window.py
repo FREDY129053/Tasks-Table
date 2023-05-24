1cffac411398d5dc9b3b383b3271315c06d3fd9a
FREDY129053 <83633741+FREDY129053@users.noreply.github.com>
2023-05-25 01:31:24 +1000

    Вывод моих досок

--- main_window.py
+++ main_window.py
@@ -2,6 +2,7 @@ import add_table_window
 
 import sys
 import mysql
+import sign_up_window, log_in_window
 
 from secrets import compare_digest
 from PyQt6.QtGui import QAction
@@ -33,6 +34,21 @@ class Main(QMainWindow):
         loadUi("Users_Interfaces/main_window.ui", self)
 
         self.setWindowTitle("Main")
+
+        # стиль меню
+        my_menu = self.findChild(QMenu, 'menu')
+        my_menu_style = "QMenu::item:selected { background-color: #6757A5; color: #FFFFFF; }"
+        my_menu.setStyleSheet(my_menu_style)
+        my_menu.setMouseTracking(True)
+
+        my_menu2 = self.findChild(QMenu, 'menu_2')
+        my_menu_style2 = "QMenu::item:selected { background-color: #6757A5; color: #FFFFFF; }"
+        my_menu2.setStyleSheet(my_menu_style2)
+        my_menu2.setMouseTracking(True)
+        #
+
+        self.my_desks = self.findChild(QAction, 'my_desks')
+        self.my_desks.triggered.connect(self.show_my_desks)
 
         self.update_as = self.findChild(QAction, 'update_act_2')
         self.update_as.triggered.connect(self.update_window)
@@ -72,7 +88,6 @@ class Main(QMainWindow):
             font: 600 13px "Work Sans";
             """)
 
-            # print(tables[i][1])
             new.setText(f"{tables[i][1]}")
             new.setObjectName(f"{tables[i][1]}")
             new.setFixedWidth(250)
@@ -136,6 +151,52 @@ class Main(QMainWindow):
         for i, button in enumerate(self.buttons):
             button.clicked.connect(lambda _, index=i: self.show_window(self.buttons[index]))
 
+    def show_my_desks(self):
+        self.scroll_area.takeWidget()
+
+        scroll_layout = QGridLayout()
+        scroll_widget = QWidget()
+
+        # виджет-пустышка для добавления в конец, чтобы место занимал
+        empty_widget = QLabel()
+        empty_widget.setFixedSize(100, 10)
+
+        database.commit()
+        author = sign_up_window.curr() if sign_up_window.curr() is not None else log_in_window.curr()
+        cursor.execute(f"SELECT * FROM tables WHERE author_login = '{author.username}'")
+        tables = cursor.fetchall()
+
+        self.buttons = []
+        for i in range(len(tables)):
+            new = QPushButton()
+
+            new.setStyleSheet("""
+                            background-color: rgb(255, 255, 255);
+                            border-radius: 10px;
+                            border-width: 2px 2px 2px 2px;
+                            border-style: solid;
+                            border-color: #6757A5;
+                            font: 600 13px "Work Sans";
+                            """)
+
+            # print(tables[i][1])
+            new.setText(f"{tables[i][1]}")
+            new.setObjectName(f"{tables[i][1]}")
+            new.setFixedWidth(250)
+            new.setFixedHeight(80)
+            self.buttons.append(new)
+
+            scroll_layout.addWidget(new, i // 2, i % 2)
+
+            if i + 1 >= len(tables):
+                scroll_layout.addWidget(empty_widget)
+
+            scroll_widget.setLayout(scroll_layout)
+            self.scroll_area.setWidget(scroll_widget)
+
+        for i, button in enumerate(self.buttons):
+            button.clicked.connect(lambda _, index=i: self.show_window(self.buttons[index]))
+
     def show_window(self, title):
         self.new_window = QWidget()
 
@@ -145,25 +206,25 @@ class Main(QMainWindow):
         self.window.show()
 
     # заполнение таблицы данными
-    def load_data(self, table):
-        tasks = [{"name": "Посрать", "age": "Покупка туалетки", "address": "Срать"},
-                 {"name": "Mark", "age": "18", "address": "Alabama"},
-                 {"name": "Посрать", "age": "Покупка туалетки", "address": ""},
-                 {"name": "Посрать", "age": "Покупка туалетки", "address": ""},
-                 {"name": "Посрать", "age": "Покупка туалетки", "address": "Обосраться"},
-                 {"name": "Посрать", "age": "Покупка туалетки", "address": ""},
-                 {"name": "Посрать", "age": "Покупка туалетки", "address": "Отложить кучу"},
-                 {"name": "Посрать", "age": "Покупка туалетки", "address": ""},
-                 {"name": "Посрать", "age": "Покупка туалетки", "address": ""},
-                 {"name": "Посрать", "age": "Покупка туалетки", "address": "Надристать"}]
-        row = 0
-        table.setRowCount(len(tasks))
-
-        for task in tasks:
-            table.setItem(row, 0, QtWidgets.QTableWidgetItem(task["name"]))
-            table.setItem(row, 1, QtWidgets.QTableWidgetItem(task["age"]))
-            table.setItem(row, 2, QtWidgets.QTableWidgetItem(task["address"]))
-            row += 1
+    # def load_data(self, table):
+    #     tasks = [{"name": "Посрать", "age": "Покупка туалетки", "address": "Срать"},
+    #              {"name": "Mark", "age": "18", "address": "Alabama"},
+    #              {"name": "Посрать", "age": "Покупка туалетки", "address": ""},
+    #              {"name": "Посрать", "age": "Покупка туалетки", "address": ""},
+    #              {"name": "Посрать", "age": "Покупка туалетки", "address": "Обосраться"},
+    #              {"name": "Посрать", "age": "Покупка туалетки", "address": ""},
+    #              {"name": "Посрать", "age": "Покупка туалетки", "address": "Отложить кучу"},
+    #              {"name": "Посрать", "age": "Покупка туалетки", "address": ""},
+    #              {"name": "Посрать", "age": "Покупка туалетки", "address": ""},
+    #              {"name": "Посрать", "age": "Покупка туалетки", "address": "Надристать"}]
+    #     row = 0
+    #     table.setRowCount(len(tasks))
+    #
+    #     for task in tasks:
+    #         table.setItem(row, 0, QtWidgets.QTableWidgetItem(task["name"]))
+    #         table.setItem(row, 1, QtWidgets.QTableWidgetItem(task["age"]))
+    #         table.setItem(row, 2, QtWidgets.QTableWidgetItem(task["address"]))
+    #         row += 1
 
     def add_table(self):
         self.main = add_table_window.AddTable()
